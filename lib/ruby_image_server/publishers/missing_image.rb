e0627e2266e65f91088d2ecb3170c94eff712fc1
Matt Camuto & Richard Millan <matt+richard@wanelo.com>
2015-03-18 15:10:36 -0700

    Add ‘id’ field to publish payload.

--- lib/ruby_image_server/publishers/missing_image.rb
+++ lib/ruby_image_server/publishers/missing_image.rb
@@ -3,15 +3,17 @@ module RubyImageServer
     class MissingImage
       include Lapine::Publisher
 
-      def initialize(namespace: nil, image_hash: nil)
+      def initialize(namespace: nil, image_hash: nil, id: nil)
         @namespace = namespace
         @image_hash = image_hash
+        @id = id
       end
 
       def to_hash
         {
           namespace: @namespace,
-          image_hash: @image_hash
+          image_hash: @image_hash,
+          id: @id,
         }
       end
     end
--- spec/unit/lib/s3/missing_image_finder_spec.rb
+++ spec/unit/lib/s3/missing_image_finder_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe RubyImageServer::S3::MissingImageFinder do
   let(:hashies) do
     [
       {
-        namespace: 'p',
+        namespace: 'p1',
         image_hash: 'poo'
       },
       {
-        namespace: 'p',
-        image_hash: 'poo'
+        namespace: 'p2',
+        id: 123
       }
     ]
   end
@@ -27,18 +27,28 @@ RSpec.describe RubyImageServer::S3::MissingImageFinder do
 
     context 'when there is data in S3' do
       before do
-        s3.stub_responses(:list_objects, contents: [ { key: 'key1' }, { key: 'key2' } ])
+        s3.stub_responses(:list_objects, contents: [{key: 'key1'}, {key: 'key2'}])
         s3.stub_responses(:get_object, body: StringIO.new('string from log'))
 
         allow(RubyImageServer::S3::FastlyLogParser).to receive(:new) { double(image_hashes: hashies) }
       end
 
-      it 'returns an array of image data hashes' do
+      it 'returns an array of hashes' do
         expect(subject.image_data).to be_a_kind_of(Array)
-        subject.image_data.each do |datum|
-          expect(datum[:namespace]).to eq('p')
-          expect(datum[:image_hash]).to eq('poo')
-        end
+      end
+
+      it 'matches items with image hash' do
+        datum = subject.image_data[0]
+        expect(datum[:namespace]).to eq('p1')
+        expect(datum[:image_hash]).to eq('poo')
+        expect(datum[:id]).to eq(nil)
+      end
+
+      it 'matches item with id' do
+        datum = subject.image_data[1]
+        expect(datum[:namespace]).to eq('p2')
+        expect(datum[:image_hash]).to eq(nil)
+        expect(datum[:id]).to eq(123)
       end
     end
   end
